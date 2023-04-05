3566aea91d4df72e0da9fd6448304df20850f425
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-04-05 18:59:16 +0100

    ...

--- Web3/hardhat.config.ts
+++ Web3/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const OP_GOERLI_URL = process.env.OP_GOERLI_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+if (PRIVATE_KEY === undefined) {
+  throw new Error("Please set your PRIVATE_KEY in the .env file");
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    op_goerli: {
+      url: OP_GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
--- Web3/scripts/deployWhitelist.ts
+++ Web3/scripts/deployWhitelist.ts
@@ -1,23 +1,16 @@
-import { ethers } from "hardhat";
+// Contract Deployed to OP_GOERLI: 0xa48D89457a94224214Da77931a1AC2FFa5bb2e30
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const hre = require("hardhat");
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+async function mainDeploy() {
+    // Deploy the contract
+    const Whitelist = await hre.ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy();
+    await whitelist.deployed();
+    console.log("Whitelist deployed to:", whitelist.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+mainDeploy().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });    
