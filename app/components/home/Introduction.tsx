ee0b18329fe6597162553526b919acd7d36053cc
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-06-10 23:59:24 +0100

    re-think

--- app/components/home/Introduction.tsx
+++ app/components/home/Introduction.tsx
@@ -5,6 +5,9 @@ const HomeView: React.FC = () => {
     return (
         <section className='h-[850px]'>
             <section className=''>
+                <p>Hey, I'm Lewis...</p>
+
+                <p>A full stack developer who loves to build websites and applications that users will love.</p>
                 
             </section>
         </section>
