7e72df4a9f077178fab09fad7a2558f36b962b98
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-06-30 21:40:44 +0100

    Working tooltip

---
+++ components/global/Tooltip.tsx
@@ -0,0 +1,20 @@
+import React, { FC, PropsWithChildren, ReactNode } from 'react';
+
+type OwnProps = PropsWithChildren & {
+    tooltipText: string;
+};
+
+const Tooltip: FC<OwnProps> = ({
+    tooltipText,
+    children
+}) => {
+
+  return (<>
+    {/* Section that is wrapped by the tooltip */}
+    <section className='relative inline-block'>
+        <section>{children}</section>
+    </section>
+  </>)
+}
+
+export default Tooltip;
